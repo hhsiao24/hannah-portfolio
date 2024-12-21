743d0e28a737e350ee63262234872d9e2ff06706
Hannah Hsiao <hhsiao24@gmail.com>
2024-12-20 22:57:58 -0800

    setup theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,44 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    darkPurple: [
+      "#f1f1f9",
+      "#dfdfeb",
+      "#bdbbd9",
+      "#9995c7",
+      "#7a75b8",
+      "#6661af",
+      "#5d56ac",
+      "#4d4797",
+      "#443f87",
+      "#393678"
+    ],
+    fancyBlue: [
+      "#eef6fc",
+      "#dceaf3",
+      "#b3d3e9",
+      "#88bce0",
+      "#66a8d8",
+      "#529bd3",
+      "#4695d2",
+      "#3881bb",
+      "#2d73a7",
+      "#196394"
+    ],
+    greenBlue: [
+      "#e8f9fd",
+      "#dcedf1",
+      "#bdd7dd",
+      "#9bc0ca",
+      "#7eadb9",
+      "#6ba1af",
+      "#5f9baa",
+      "#4d8796",
+      "#407986",
+      "#2b6977"
+    ]
+  },
+  primaryColor: "greenBlue",
+  fontFamily: "poppins, sans-serif",
 });
