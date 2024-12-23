40d9af86a6bf89f9a5b078c9cb59ad5a60bdc313
Hannah Hsiao <hhsiao24@gmail.com>
2024-12-23 15:08:11 -0800

    adjusted ui of cards

--- src/components/Projects/Label.tsx
+++ src/components/Projects/Label.tsx
@@ -1,54 +1,174 @@
 import React from 'react';
-import {Box, Card, Group, Image} from '@mantine/core';
+import { Badge, Box, Button, Card, Grid, HoverCard, Image, List, Text, Title } from '@mantine/core';
 
 export const Label = ({
   x,
   y,
-  children,
-  direction = 'right',
+  title,
+  descriptionItems,
+  skills,
   img,
+  direction = 'right',
+  buttons = 1,
+  button1Text = 'Button 1',
+  button1Link = '#',
+  button2Text = 'Button 2',
+  button2Link = '#',
 }: {
   x: number;
   y: number;
-  children: React.ReactNode;
-  direction: string;
+  title: string;
+  descriptionItems: string[];
+  skills: string[];
   img: string;
+  direction: string;
+  buttons?: number;
+  button1Text?: string;
+  button1Link?: string;
+  button2Text?: string;
+  button2Link?: string;
 }) => {
   const isLeft = direction === 'left';
 
   return (
-      <Box
-          p={0}
-          w={375}
-          style={{
-              display: 'flex',
-              position: 'absolute',
-              top: `calc(50% - ${y}%)`,
-              left: `calc(50% - ${x}%)`,
-              flexDirection: isLeft ? 'row-reverse' : 'row',
-          }}
-      >
+    <Box
+      style={{
+        display: 'flex',
+        position: 'absolute',
+        top: `calc(50% - ${y}%)`,
+        left: `calc(50% - ${x}%)`,
+        flexDirection: isLeft ? 'row-reverse' : 'row',
+      }}
+    >
       <svg height={100} width={200} style={{ transform: isLeft ? 'scaleX(-1)' : undefined }}>
         <circle cx={5} cy={95} r={5} fill="white" />
         <line x1={5} y1={95} x2={100} y2={0} style={{ stroke: 'white', strokeWidth: 1 }} />
         <line x1={100} y1={0} x2={200} y2={0} style={{ stroke: 'white', strokeWidth: 1 }} />
       </svg>
-      <Card
-        shadow="sm"
-        padding="lg"
-        radius="md"
-        withBorder
-        style={{
-          backgroundColor: 'rgba(18, 17, 38, 0.5)',
-          backdropFilter: 'blur(10px)',
-          transition: 'box-shadow 0.5s',
-        }}
-      >
-        <Card.Section>
-          <Image src={img} height={160} />
-        </Card.Section>
-        {children}
-      </Card>
+
+      <HoverCard shadow="md" width={400} openDelay={200} closeDelay={200}>
+        <HoverCard.Target>
+          <Card
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            w={300}
+            withBorder
+            style={{
+              backgroundColor: 'rgba(18, 17, 38, 0.5)',
+              backdropFilter: 'blur(10px)',
+              cursor: 'pointer',
+              transition: 'box-shadow 0.5s',
+              width: 300,
+            }}
+          >
+            <Text size="lg" fw="bold">
+              {title}
+            </Text>
+
+            <div
+              style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)', margin: '0.5rem 0' }}
+            />
+
+            <Box
+              style={{
+                display: 'block',
+                marginTop: '0.5rem',
+                marginBottom: '0.5rem',
+              }}
+            >
+              <Title size="sm" fw="bold">
+                Technologies
+              </Title>
+            </Box>
+
+            <Box
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: '0.5rem',
+              }}
+            >
+              {skills.map((skill, index) => (
+                <Badge key={index} variant="filled" style={{ backgroundColor: '#66a8d8' }}>
+                  {skill}
+                </Badge>
+              ))}
+            </Box>
+          </Card>
+        </HoverCard.Target>
+
+        <HoverCard.Dropdown>
+          <Card
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
+            style={{
+              backgroundColor: 'rgba(18, 17, 38, 0.5)',
+              backdropFilter: 'blur(15px)',
+            }}
+          >
+            <Card.Section>
+              <Image src={img} height={160} />
+            </Card.Section>
+            <Text size="lg" fw="bold" style={{ marginTop: '1rem' }}>
+              {title}
+            </Text>
+
+            <List size="sm" style={{ marginTop: '0.5rem', color: 'white' }}>
+              {descriptionItems.map((item, index) => (
+                <List.Item key={index}>{item}</List.Item>
+              ))}
+            </List>
+
+            <div
+              style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)', margin: '0.5rem 0' }}
+            />
+
+            <Box
+              style={{
+                display: 'block',
+                marginTop: '0.5rem',
+                marginBottom: '0.5rem',
+              }}
+            >
+              <Title size="sm" fw="bold">
+                Technologies
+              </Title>
+            </Box>
+
+            <Box
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: '0.5rem',
+              }}
+            >
+              {skills.map((skill, index) => (
+                <Badge key={index} variant="filled" style={{ backgroundColor: '#66a8d8' }}>
+                  {skill}
+                </Badge>
+              ))}
+            </Box>
+
+            <Grid p={2} mt={10}>
+              <Grid.Col span={6}>
+                <Button variant="outline" component="a" href={button1Link} fullWidth>
+                  {button1Text}
+                </Button>
+              </Grid.Col>
+              {buttons === 2 && (
+                <Grid.Col span={6}>
+                  <Button variant="outline" component="a" href={button2Link} fullWidth>
+                    {button2Text}
+                  </Button>
+                </Grid.Col>
+              )}
+            </Grid>
+          </Card>
+        </HoverCard.Dropdown>
+      </HoverCard>
     </Box>
   );
 };
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,32 +1,68 @@
-import outline from '/src/assets/robotOutline.png';
-import { Container, Image, Stack, Title, Text } from '@mantine/core';
-import { Label } from './Label';
 import breadboard from '/src/assets/breadboardBakery.jpg';
+import outline from '/src/assets/robotOutline.png';
+import { Container, Image, List, Stack, Text, Title } from '@mantine/core';
+import { Label } from './Label';
+import formFit from '/public/formfit.png'
 
 export const Projects = () => {
   return (
-    <Container id="projects" fluid m={0} style={{ position: 'relative', backgroundColor: '#002d44' }}>
-      <Title w="100%" ta="center" pb={60} pt={100}>
+    <Container
+      id="projects"
+      fluid
+      m={0}
+      style={{ position: 'relative', backgroundColor: '#002d44' }}
+    >
+      <Title w="100%" ta="center" pb={10} pt={100}>
         Projects
       </Title>
+      <Text ta="center" pb={80}>
+        {' '}
+        hover for more information!
+      </Text>
       <Stack justify="center" align="center" pb={100}>
-        <Container
-          fluid
-          p={0}
-          style={{ position: 'absolute', height: '100%', width: '30%' }}
-        >
-          <Label x={1.5} y={37} direction="right" img={breadboard}>
-            <>
-              <Text fw={700} mt="md" mb="xs">Breadboard Bakery</Text>
-              <Text size="sm" c="dimmed">
-                With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-                activities on and around the fjords of Norway
-              </Text>
-            </>
-          </Label>
-          {/*<Label x={120} y={25} direction="left">*/}
-          {/*  <>babababaBABABAB</>*/}
-          {/*</Label>*/}
+        <Container fluid p={0} style={{ position: 'absolute', height: '100%', width: '30%' }}>
+          <Label
+            x={1.5}
+            y={37}
+            title="Breadboard Bakery 🍞"
+            descriptionItems={[
+              'A web application that enables users to upload images of their wired breadboards, generating' +
+                'corresponding logic circuit schematics using advanced image processing techniques',
+              'Used image processing techniques to detect and isolate breadboards from backgrounds, utilizing' +
+                'grayscale conversion, edge detection, and contour detection to identify wires and chips for schematic generation',
+              'Deployed a Flask API to facilitate communication between the frontend and backend, manage image uploads and' +
+                'displaying generate circuit schematic images',
+            ]}
+            skills={['React', 'Flask', 'OpenCV', 'Python', 'HTML', 'CSS', 'Javascript']}
+            img={breadboard}
+            direction="right"
+            buttons={2}
+            button1Text="Repository"
+            button1Link="https://github.com/hhsiao24/breadboard-analyzer"
+            button2Text="Learn More"
+            button2Link="https://devpost.com/software/breadboard-bakery"
+          />
+
+          <Label
+            x={140}
+            y={25}
+            title="Form Fit 🏋🏻‍♀️"
+            descriptionItems={[
+              '2nd place in Sports Track, 3rd place in Health Track',
+              'Web application delivering personalized exercise form feedback',
+              'Employed MediaPipe and OpenCV to analyze user’s exercise videos, creating a downloadable video visualizer' +
+              'that compares user’s exercise form to the “perfect” form',
+              'Engineered an algorithm to interpret visualizer data, providing actionable feedback for user’s improvement',
+            ]}
+            skills={['MediaPipe', 'OpenCV', 'React', 'Python', 'HTML', 'CSS', 'Javascript']}
+            img={formFit}
+            direction="left"
+            buttons={2}
+            button1Text="Repository"
+            button1Link="https://github.com/hhsiao24/FormFit"
+            button2Text="Link"
+            button2Link="https://formfit.tech/"
+          />
         </Container>
         <Image src={outline} w="30%" />
       </Stack>
