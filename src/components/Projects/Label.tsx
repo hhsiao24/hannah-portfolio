04b6a5e2b5f9a9602ed644fd457df862ba829f8b
Hannah Hsiao <hhsiao24@gmail.com>
2024-12-23 01:15:39 -0800

    added to cards - issue with card placement

--- src/components/Projects/Label.tsx
+++ src/components/Projects/Label.tsx
@@ -1,38 +1,54 @@
 import React from 'react';
-import { Card, Group } from '@mantine/core';
+import {Box, Card, Group, Image} from '@mantine/core';
 
 export const Label = ({
   x,
   y,
   children,
   direction = 'right',
+  img,
 }: {
   x: number;
   y: number;
   children: React.ReactNode;
   direction: string;
+  img: string;
 }) => {
   const isLeft = direction === 'left';
 
   return (
-    <Group
-      p={0}
-      wrap="nowrap"
-      align="start"
-      style={{
-        position: 'absolute',
-        top: `calc(50% - ${y}%)`,
-        left: `calc(50% - ${x}%)`,
-        flexDirection: isLeft ? 'row-reverse' : 'row',
-      }}
-      gap={0}
-    >
+      <Box
+          p={0}
+          w={375}
+          style={{
+              display: 'flex',
+              position: 'absolute',
+              top: `calc(50% - ${y}%)`,
+              left: `calc(50% - ${x}%)`,
+              flexDirection: isLeft ? 'row-reverse' : 'row',
+          }}
+      >
       <svg height={100} width={200} style={{ transform: isLeft ? 'scaleX(-1)' : undefined }}>
         <circle cx={5} cy={95} r={5} fill="white" />
         <line x1={5} y1={95} x2={100} y2={0} style={{ stroke: 'white', strokeWidth: 1 }} />
         <line x1={100} y1={0} x2={200} y2={0} style={{ stroke: 'white', strokeWidth: 1 }} />
       </svg>
-      <Card>{children}</Card>
-    </Group>
+      <Card
+        shadow="sm"
+        padding="lg"
+        radius="md"
+        withBorder
+        style={{
+          backgroundColor: 'rgba(18, 17, 38, 0.5)',
+          backdropFilter: 'blur(10px)',
+          transition: 'box-shadow 0.5s',
+        }}
+      >
+        <Card.Section>
+          <Image src={img} height={160} />
+        </Card.Section>
+        {children}
+      </Card>
+    </Box>
   );
 };
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,9 @@
 import outline from '/src/assets/robotOutline.png';
-import { Container, Image, Stack, Title } from '@mantine/core';
-import { useDims } from '@/hooks/useDims';
+import { Container, Image, Stack, Title, Text } from '@mantine/core';
 import { Label } from './Label';
+import breadboard from '/src/assets/breadboardBakery.jpg';
 
 export const Projects = () => {
-  const { ref, width, height } = useDims();
   return (
     <Container id="projects" fluid m={0} style={{ position: 'relative', backgroundColor: '#002d44' }}>
       <Title w="100%" ta="center" pb={60} pt={100}>
@@ -13,20 +12,21 @@ export const Projects = () => {
       <Stack justify="center" align="center" pb={100}>
         <Container
           fluid
-          ref={ref}
           p={0}
           style={{ position: 'absolute', height: '100%', width: '30%' }}
         >
-          <Label x={1.5} y={37} direction="right">
+          <Label x={1.5} y={37} direction="right" img={breadboard}>
             <>
-              <div>
-                <p style={{ fontWeight: 'bold' }}>BreadBoard Bakery</p>
-              </div>
+              <Text fw={700} mt="md" mb="xs">Breadboard Bakery</Text>
+              <Text size="sm" c="dimmed">
+                With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+                activities on and around the fjords of Norway
+              </Text>
             </>
           </Label>
-          <Label x={120} y={25} direction="left">
-            <>babababaBABABAB</>
-          </Label>
+          {/*<Label x={120} y={25} direction="left">*/}
+          {/*  <>babababaBABABAB</>*/}
+          {/*</Label>*/}
         </Container>
         <Image src={outline} w="30%" />
       </Stack>
