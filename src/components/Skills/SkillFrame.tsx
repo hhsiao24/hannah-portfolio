0a4e8c9ec1e725136f1cd6a37dd1b7ee6a4898d8
Hannah Hsiao <hhsiao24@gmail.com>
2024-12-24 23:45:01 -0800

    test mobile

--- src/components/Skills/SkillFrame.tsx
+++ src/components/Skills/SkillFrame.tsx
@@ -3,17 +3,17 @@ import { Box, Card, Grid, Group, Image, Text } from '@mantine/core';
 
 export const SkillFrame = ({ skill, img }: { skill: string; img: string }) => {
   return (
-    <Grid.Col span={{ base: 12, xs: 6, sm: 4, md: 3 }}>
-      <Card shadow="xs" p="xs" withBorder style={{ backgroundColor: 'rgba(18, 17, 38, 0.5)' }}>
-        <Group wrap="nowrap">
-          <Box>
-            <Image src={img} h={35} />
-          </Box>
-          <Text ta="left" style={{ flex: 1 }}>
-            {skill}
-          </Text>
-        </Group>
-      </Card>
-    </Grid.Col>
+      <Grid.Col span={{ base: 12, xs: 6, sm: 4, md: 3 }}>
+        <Card shadow="xs" p="xs" withBorder style={{ backgroundColor: 'rgba(18, 17, 38, 0.5)' }}>
+          <Group wrap="nowrap">
+            <Box style={{ height: 35, width: 35, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Image src={img} style={{ maxHeight: '100%', maxWidth: '100%', objectFit: 'contain' }} />
+            </Box>
+            <Text ta="left" style={{ flex: 1 }}>
+              {skill}
+            </Text>
+          </Group>
+        </Card>
+      </Grid.Col>
   );
 };
